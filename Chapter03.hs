29f26b229bc830e7f5fa8d8fe1a8095d4c71c68a
Patrick Huffer <rhywun@rhywun.com>
2018-06-06 22:27:27 -0400

    End of Chapter02

--- Chapter03.hs
+++ Chapter03.hs
@@ -1,9 +1,10 @@
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase s = [ c | c <- s, c `elem` ['A'..'Z']]
 
-addThree :: Int -> Int -> Int -> Int  
-addThree x y z = x + y + z  
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
 
 factorial n = product [1..n]
 
 circumference r = 2 * pi * r
+ 
